b8775bb3db03f10d5ef1525f0da021281deffaeb
Gelson Gerson Celestino <103900087+GelsonGerson@users.noreply.github.com>
2022-10-13 08:54:49 +0200

    updated

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// }
 
-window.onload = setup;
+// window.onload = setup;
+
+function getAllShows() {
+  return [
+    {
+      "id": 1632,
+      "url": "http://www.tvmaze.com/shows/1632/horatio-hornblower",
+      "name": "Horatio Hornblower",
+      "type": "Scripted",
+      "language": "English",
+      "genres": [
+        "Drama",
+        "Action",
+        "Adventure"
+      ],
+    }
+  }
