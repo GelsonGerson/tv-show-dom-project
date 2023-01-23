bca90811d2a23c2b4df6247996ee8940b6e22f2f
Gelson Gerson Celestino <103900087+GelsonGerson@users.noreply.github.com>
2023-01-23 08:57:58 +0200

    Episode edited

--- script.js
+++ script.js
@@ -24,5 +24,29 @@ function getAllShows() {
         "Action",
         "Adventure"
       ],
+    },
+    {
+    id: 82,
+    url: "http://www.tvmaze.com/shows/82/game-of-thrones",
+    name: "Game of Thrones",
+    type: "Scripted",
+    language: "English",
+    genres: ["Drama", "Adventure", "Fantasy"],
+    status: "Ended",
+    runtime: 60,
+    premiered: "2011-04-17",
+    officialSite: "http://www.hbo.com/game-of-thrones",
+    schedule: { time: "21:00", days: ["Sunday"] },
+    rating: { average: 9.1 },
+    weight: 97,
+    network: {
+      id: 8,
+      name: "HBO",
+      country: {
+        name: "United States",
+        code: "US",
+        timezone: "America/New_York",
+      },
     }
-  }
+  ]
+  };
